e53288aa2faf616ceb4304a1a85909d1eb7fd6d8
Vance-Joseph <vance.joseph.jv@gmail.com>
2013-05-09 15:27:26 -0500

    Create game.js

---
+++ game.js
@@ -0,0 +1,7 @@
+//game constructor
+function game(name,drinktype,difficulty,description) {
+this.gameName : name,
+this.drinkType : drinktype,
+this.difficulty : difficulty,
+this.description : description
+}
